068ebb6374a96978151f4c5632f7152cffff9fc5
Jon Wood <jonmwood@gmail.com>
2019-01-18 13:06:25 -0800

    updated config.yml and template for a Bible Study Part for Netlify CMS

---
+++ src/templates/bible-study-part.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import TemplateWrapper from '../components/Layout'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import styled from 'styled-components'
+
+const bibleStudyImageStyle = {
+    'borderRadius': '5px',
+    'width': '20vw',
+    'height': '20vw',
+    'margin': '0 auto'
+}
+
+
+
+const StyledDate = styled.p`
+    font-weight: 700;
+    color: #555;
+`
+
+export default class BibleStudy extends Component {
+    render() {
+        const { markdownRemark } = this.props.data
+        return (
+            <div>
+                <TemplateWrapper>
+                    <h1>{markdownRemark.frontmatter.title}</h1>
+                    <StyledDate>{markdownRemark.frontmatter.date}</StyledDate>
+                    <div dangerouslySetInnerHTML={{
+                        __html: markdownRemark.html
+                    }} />
+                    <PreviewCompatibleImage imageInfo={markdownRemark.frontmatter} imageStyle={bibleStudyImageStyle} />
+
+
+                </TemplateWrapper>
+            </div>
+        )
+    }
+}
+
+
+export const query = graphql`
+    query BibleStudyPartQuery($slug: String!) {
+        markdownRemark(fields: {
+            slug: {
+                eq: $slug
+            }
+        }) {
+            fields {
+                slug
+            }
+            id
+            html
+            frontmatter {
+                title
+                date(formatString: "MMMM DD YYYY")
+                image {
+                    childImageSharp {
+                        fluid(maxWidth: 1000) {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
